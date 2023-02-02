6c27f3ad623bf7c2684e6e86feae52ace509d184
Crowdbotics <team@crowdbotics.com>
2023-02-02 01:14:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/selena_toca_life_wo_38653/MainActivity.java
@@ -0,0 +1,15 @@
+package com.selena_toca_life_wo_38653;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "selena_toca_life_wo_38653";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "selena-toca-life-wo-38653",
+  url: "https://selena-toca-life-wo-38653.botics.co",
+  api: "https://selena-toca-life-wo-38653.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
